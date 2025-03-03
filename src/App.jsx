905e25189ecfb291964ef8fee02301ae5769590c
Chidera Ulu <chideraulu@gmail.com>
2025-03-03 17:13:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <div className="pattern" />
+
+        <div className="wrapper">
+          <header>
+            <img src="./hero-img.png" alt="" />
+            <h1>Find 
+              <span className="text-gradient"> Movies</span> 
+              You'll Enjoy Without the Hassle
+            </h1>
+          </header>
+
+          <p>Search</p>
+        </div>
+    </main>
+  )
+}
+
+export default App
